c38255d897bb9740a2342589a71d75be57f8a2b2
shuhei.kikuchi <shuhei.kikuchi@jp.oro.com>
2016-12-20 00:56:56 +0900

    Annotate components

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
 const gulp = require('gulp');
+const fs = require('fs');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
-const concat = require('gulp-concat');
-const cache = require('gulp-cached');
 const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const discardComments = require('postcss-discard-comments');
+const styleGuide = require('postcss-style-guide');
 const cssnano = require('cssnano');
-const runSequence = require('run-sequence');
+const cheerio = require('cheerio');
 const browserSync = require('browser-sync').create();
-const { execCommand } = require('./scripts/helpers');
+
+// Extra styles to apply styleguide page.
+// TODO: move seperate file and importing it
+const extraStyles = `
+html {
+    overflow-y: hidden;
+}
+code {
+    font-family: 'Inconsolata', 'Consolas', monospace !important;
+}
+.cf:before,
+.cf:after {
+    content: " ";
+    display: table;
+}
+.cf:after {
+    clear: both;
+}
+.box + .box {
+    padding-top: 40px;
+    border-top: 1px solid #f1f1f1;
+}
+`;
 
 gulp.task('sass', (cb) => {
     return gulp.src('./scss/orotter-bootstrap.scss')
@@ -20,10 +42,19 @@ gulp.task('sass', (cb) => {
             ],
         }).on('error', sass.logError))
         .pipe(postcss([
-            discardComments(),
             autoprefixer({ browsers: ['last 2 versions'] }),
+            styleGuide({
+                project: 'oRotter bootstrap',
+                dest: 'styleguide/index.html',
+                theme: 'psg-theme-sassline',
+                themePath: 'node_modules/psg-theme-sassline'
+            }),
         ]))
-        .pipe(gulp.dest('./css'));
+        .pipe(postcss([
+            discardComments(),
+        ]))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('sass:min', (cb) => {
@@ -42,29 +73,28 @@ gulp.task('sass:min', (cb) => {
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch:sass', (cb) => {
+gulp.task('watch:sass', ['sass'], (cb) => {
     return gulp.watch(['./scss/**/*.scss'], ['sass']);
 });
 
-gulp.task('build:styleguide', (cb) => {
-    setTimeout(() => {
-    execCommand('npm run styleguide')
-        .catch(err => console.error(err))
-        .then(cb);
-    }, 0);
-});
-
-gulp.task('serve', ['build:sass', 'build:styleguide'], () => {
+gulp.task('styleguide', ['sass'], (cb) => {
     browserSync.init({
         server: './styleguide',
-        serveStatic: [{
-            route: '/css',
-            dir: 'css'
-        }]
+        middleware: [
+            {
+                route: '/',
+                handle: function(req, res, next) {
+                    // Control some contents of the styleguide html.
+                    const indexHtml = fs.readFileSync('./styleguide/index.html', 'utf-8');
+                    const $ = cheerio.load(indexHtml);
+                    // Append custom css to the document.
+                    $('head').append(`<style>${extraStyles}</style>`);
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end($.html());
+                }
+            }
+        ]
     });
 
-    gulp.watch(['./scss/**/*.scss'], () => {
-        runSequence('build:sass');
-    });
-    gulp.watch(['./scss/**/*.scss'], ['build:styleguide']);
+    gulp.watch(['./scss/**/*.scss'], ['sass']);
 });
